5ff1cbf6afba86281717a2b0f3027f05faa93093
fang-gn <fangguoneng@outlook.com>
2025-03-20 13:38:53 +0800

    a database demo

--- TestForAICodeReview/TestForAICodeReview/Program.cs
+++ TestForAICodeReview/TestForAICodeReview/Program.cs
@@ -1,10 +1,73 @@
-﻿namespace TestForAICodeReview
+﻿using System;
+using System.Data.SQLite;
+
+namespace TestForAICodeReview
 {
-    internal class Program
+    class Program
     {
+        //数据库连接
+        SQLiteConnection m_dbConnection;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program p = new Program();
+        }
+
+        public Program()
+        {
+            createNewDatabase();
+            connectToDatabase();
+            createTable();
+            fillTable();
+            printHighscores();
+        }
+
+        //创建一个空的数据库
+        void createNewDatabase()
+        {
+            SQLiteConnection.CreateFile("MyDatabase.db");//可以不要此句
+        }
+
+        //创建一个连接到指定数据库
+        void connectToDatabase()
+        {
+            m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.db;Version=3;");//没有数据库则自动创建
+            m_dbConnection.Open();
+        }
+
+        //在指定数据库中创建一个table
+        void createTable()
+        {
+            string sql = "create table  if not exists highscores (name varchar(20), score int)";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+        }
+
+        //插入一些数据
+        void fillTable()
+        {
+            string sql = "insert into highscores (name, score) values ('Me', 3000)";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            sql = "insert into highscores (name, score) values ('Myself', 6000)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            sql = "insert into highscores (name, score) values ('And I', 9001)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+        }
+
+        //使用sql查询语句，并显示结果
+        void printHighscores()
+        {
+            string sql = "select * from highscores order by score desc";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+                Console.WriteLine("Name: " + reader["name"] + "\tScore: " + reader["score"]);
+            Console.ReadLine();
         }
     }
 }
